5e9e7730d1adac354f51a35532d867161328c200
mirage <119869686+ClementBobin@users.noreply.github.com>
2026-08-30 14:17:38 +0200

    Add new animation configurations and update paths
    - Updated animation source paths in animations.nix to use relative paths.
    - Added multiple new animation configuration files including LimeFrenzy, Classic, Diablo-1, Diablo-2, and others.
    - Introduced a theme configuration for animations.
    - Created vertical animation configuration with specific easing functions and styles.
    - Included disable and dynamic animation configurations for enhanced user customization.

--- hydenix/modules/hm/hyprland/animations.nix
+++ hydenix/modules/hm/hyprland/animations.nix
@@ -46,7 +46,7 @@ in
             }
           else
             {
-              source = "${pkgs.hyde}/Configs/.config/hypr/animations/${cfg.animations.preset}.conf";
+              source = ./hypr/animations/${cfg.animations.preset}.conf;
               force = true;
               mutable = true;
             };
@@ -64,7 +64,7 @@ in
               }
             else
               {
-                source = "${pkgs.hyde}/Configs/.config/hypr/animations/${preset}.conf";
+                source = ./hypr/animations/${preset}.conf;
               };
         }) animationPresets
       ))
