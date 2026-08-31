786e941f87d067754f9a6db7b032ee16d59e3b30
mirage <119869686+ClementBobin@users.noreply.github.com>
2026-08-31 16:16:42 +0200

    fix: update theme repository revisions for 1-Bit, Breezy Autumn, Cosmic Blue, and Crimson-Blue

--- hydenix/sources/themes/Cosmic-Blue.nix
+++ hydenix/sources/themes/Cosmic-Blue.nix
@@ -4,7 +4,7 @@ mkTheme rec {
   src = pkgs.fetchFromGitHub {
     owner = "Maroc02";
     repo = "Cosmic-Blue";
-    rev = "f5e0e85d333f8ae93caa262f46303efe4c35c5a8";
+    rev = "ad8a9a50684f9ab6dbd29cd98b6c2a3f457347f3";
     name = name;
     sha256 = "sha256-ed9QxS4onniN+kABewK9kBRu9xFMvHU6em+QcP2GrFM=";
   };
