7521706f235585651cf81355933d62995b32506e
mirage <119869686+ClementBobin@users.noreply.github.com>
2026-09-07 13:05:18 +0200

    fix: update flake.lock for hyde-config and include in overlay.nix packages

--- hydenix/sources/overlay.nix
+++ hydenix/sources/overlay.nix
@@ -19,6 +19,7 @@ let
     hydenix-themes = callPackage ./themes/default.nix { };
     hyq = inputs.hyq.packages.${prev.stdenv.hostPlatform.system}.default;
     hydectl = inputs.hydectl.packages.${prev.stdenv.hostPlatform.system}.default;
+    hyde-config = inputs.hyde-config.packages.${prev.stdenv.hostPlatform.system}.default;
   };
 in
 packages
