721a38a16eb4a28fa9a4b39c4f7c48aa9952e522
mirage <119869686+ClementBobin@users.noreply.github.com>
2026-09-26 14:50:37 +0200

    fix: update flake.lock and remove nixpkgs-unstable input from flake.nix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     # Hydenix's nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/release-26.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
